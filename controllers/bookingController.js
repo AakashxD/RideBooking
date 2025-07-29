17edf65ea32ffcdd5daae014055307c63331f94c
Aakash <axkashsingh@gmail.com>
2025-07-29 08:42:23 +0530

    fix api issue

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,33 +1,54 @@
-const Booking=require('../models/booking')
-const bookingService=require('../services/bookingService');
+const bookingService = require('../services/bookingService');
+const { io } = require('../index');
 const locationService = require('../services/locationService');
-const createBooking=(io)=>async(req,res)=>{
-        const {source ,destination}=req.body;
-        const booking =await bookingService.createBooking({passengerId:req.user._id});
 
-        const nearByDrivers=await bookingService.findNearByDrivers(source);
-        const drivereIds=[];
-        for(const driver of nearByDrivers){
-            const driverSocketId=await locationService.getDriverSocket(driver[0]);
-            if(driverSocketId){
-                drivereIds.push(driver[0]);
-                io.to(driverSocketId).emit('newBooking',{bookingId:booking._id,source,destination,fare:booking.fare});
-            }
 
-            // get socket ID
-            // emit notification -> alert
+const createBooking = (io) => async (req, res) => {
+  try {
+    const { source, destination } = req.body;
+    const booking = await bookingService.createBooking({ passengerId: req.user._id, source, destination });
+    const driverIds = [];
+
+    const nearbyDrivers = await bookingService.findNearbyDrivers(source);
+    for (const driver of nearbyDrivers) {
+        const driverSocketId = await locationService.getDriverSocket(driver[0]);
+        if (driverSocketId) {
+          driverIds.push(driver[0]);
+          io.to(driverSocketId).emit('newBooking', { bookingId: booking._id, source, destination, fare: booking.fare });
         }
-        await locationService.storeNotifiedDrivers({bookingId,driverIds}){
-            for(const driverId of driverIds){
-                const addedCount=await redisClinet.sAdd(`notifiedID`)
-            }
-        }
+    }
 
-}
-const confirmBooking=async(io)=>{
+    await locationService.storeNotifiedDrivers(booking._id, driverIds);
+    res.status(201).send({data:booking, success: true, error: null, message: "successfully created booking"});
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+const confirmBooking = (io) => async (req, res) => {
+  try {
+    const { bookingId } = req.body;
+    const booking = await bookingService.assignDriver(bookingId, req.user._id);
     
-}
+    const notifiedDriverIds = await locationService.getNotifiedDrivers(bookingId);
 
-module.exports={
-    createBooking,confirmBooking
-}
+    for (const driverId of notifiedDriverIds) {
+      const driverSocketId = await locationService.getDriverSocket(driverId);
+      if (driverSocketId) {
+        if (driverId === req.user._id) {
+          io.to(driverSocketId).emit('rideConfirmed', { bookingId, driverId: req.user._id });
+        } else {
+          io.to(driverSocketId).emit('removeBooking', { bookingId });
+        }
+      }
+    }
+
+    res.status(201).send({data:booking, success: true, error: null, message: "successfully confirmed booking"});
+
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+
+module.exports = { createBooking, confirmBooking };
